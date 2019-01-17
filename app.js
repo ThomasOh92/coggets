f05b30a2eda71154a049a642e6cdb0f981fdce37
thomdebomb <ohcsthomas@gmail.com>
2019-01-17 23:48:12 +0800

    Removed older comments on scraping

--- app.js
+++ app.js
@@ -49,35 +49,7 @@ app.post('/', function(req, res) {
 
     ()
 
-
   })
-
-
-/*
-  app.get('/scrape', function(req, res){
-
-    request(articleToScrape, function(error, response, html){
-      if(!error){
-        var $ = cheerio.load(html);
-
-        var title, release, rating;
-        var json = { title : "", image : "", content : ""};
-
-        $('.header').filter(function(){
-          var data = $(this);
-          title = data.children().first().text();  //to check (This is the DOM travesal, need to learn more)
-          image = data.children().first().image(); // to check (This is the DOM travesal, need to learn more)
-          content = data.children().first().content(); //to check (This is the DOM travesal, need to learn more)
-
-          json.title = title;
-          json.image = image;
-          json.content = content;
-        })
-      }
-    })
-
-  })
-*/
 
 app.listen(3000, function(){
   console.log('Server started on port 3000')
